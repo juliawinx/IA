779064fad1a9cceacdf3d07347a407252c3f6be8
juliawinx <114679318+juliawinx@users.noreply.github.com>
2024-06-13 11:56:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
 enunciado: "Qual é a sua ideia de um encontro perfeito?",
         alternativas: [
             {
