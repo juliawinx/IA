e30f6752bc7bfa8a8301ee3bddcd57d06698ccd7
juliawinx <114679318+juliawinx@users.noreply.github.com>
2024-06-13 11:53:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+enunciado: "Qual é a sua ideia de um encontro perfeito?",
+        alternativas: [
+            {
+                texto: "Uma caminhada romântica pela floresta ao pôr do sol",
+                afirmacao: "Você é atraído (a) pelo calor e pela lealdade inabalável."
+            },
+            {
+                texto: "Um jantar à luz de velas em um restaurante elegante.",
+                afirmacao: "Você é atraído(a) pelo mistério e pela elegância."
+            }
+        ]
+    },
+    {
+        enunciado: "O que te atrai mais em uma pessoa?",
+        alternativas: [
+            {
+                texto: "Um espírito aventureiro e um físico
+atlético",
+                afirmacao: "Jacob representa aventura, parceria e um amor que é sempre protetor e apaixonado."
+            },
+            {
+                texto: "Um charme misterioso e uma
+inteligência aguçada",
+                afirmacao: "Edward oferece um amor eterno e uma ligação intelectual profunda."
+            }
+        ]
+    },
+    {
+        enunciado: "Como você lida com situações difíceis?",
+        alternativas: [
+            {
+                texto: "Com paixão e determinação, nunca desistindo",
+                afirmacao: "Vocês dois compartilham uma conexão profunda com a natureza e uma energia vibrante."
+            },
+            {
+                texto: "Com calma e reflexão, pensando antes de agir",
+                afirmacao: "Vocês compartilham um gosto pelo clássico e pelo refinado, apreciando momentos tranquilos e reflexivos juntos."
+            }
+        ]
+    },
+    {
+        enunciado: "Como você se sente em relação ao sobrenatural?",
+        alternativas: [
+            {
+                texto: "Fascinado(a) e curioso (a), sempre aberto a) a novas possibilidades
+ ",
+                afirmacao: "Quer compreender tudo o que faz parte do universo, mas há uma área que costuma intrigá-lo particularmente: o sobrenatural."
+            },
+            {
+                texto: "Cético(a) mas interessado(a), gostando de explorar o desconhecido",
+                afirmacao: "Quer compreender tudo o que faz parte do universo, mas há uma área que costuma intrigá-lo particularmente: a natureza humana."
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é o seu estilo de vida?",
+        alternativas: [
+            {
+                texto: "Ativo (a) e cheio a) de energia",
+                afirmacao: "Você vai casar com Jacob, o Lobo!"
+            },
+            {
+                texto: "Tranquilo (a) e introspectivo (a)",
+                afirmacao: "Você vai casar com *Edward, o Vampiro*!"
+            }
+        ]
+    },
+];
